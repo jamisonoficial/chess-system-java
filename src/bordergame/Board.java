2820832b2528539965f473260e7f6bf61152cf92
Jamison Silva <jamisonempresas@gmail.com>
2020-07-01 18:48:00 -0300

    Moving pieces

--- src/bordergame/Board.java
+++ src/bordergame/Board.java
@@ -52,6 +52,20 @@ public class Board {
 		piece.position = position;
 	}
 	
+	//Remove a peça do tabuleiro
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	//Verifica a existência de deterinada posição no tabuleiro
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >=0 && column < columns;
