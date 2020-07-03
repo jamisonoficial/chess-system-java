2eb14c0d04d7b75050b792c9c807ec9f4e98ae8a
Jamison Silva <jamisonempresas@gmail.com>
2020-07-03 10:08:20 -0300

    Possible moves of a piece

--- src/bordergame/Piece.java
+++ src/bordergame/Piece.java
@@ -1,6 +1,6 @@
 package bordergame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -13,4 +13,25 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
+	
+	//Corresponde a uma método abstrato responsável por preencher a matriz de possibiliades de movimentos.
+	public abstract boolean[][] possibleMoves();
+	
+	//Apenas retorna o valor da célula na  matriz de possibiliades indicando se é possível ou não mover a peça para este local.
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for(int i=0; i < mat.length; i++) {
+			for(int j=0; j< mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 }
